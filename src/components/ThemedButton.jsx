22977f5d3a3cf79543b24dc23f5d9a7ffa5c3fba
Rusetska Tetiana <rusetskatata@gmail.com>
2026-05-11 12:25:02 +0200

    change css --> tailwind

--- src/components/ThemedButton.jsx
+++ src/components/ThemedButton.jsx
@@ -8,16 +8,15 @@ export default function ThemeButton() {
     const { theme, toggleTheme } = useContext(ThemeContext);
 
     return (
-       <button
-    onClick={toggleTheme}
-    className="w-10 h-10 inline-flex items-center justify-center bg-red-500"
-    style={{ flexShrink: 0 }}
->
-    <img
-        src={theme.name === "light" ? darkIcon : lightIcon}
-        alt="theme icon"
-        style={{ width: "24px", height: "24px", display: "block" }}
-    />
-</button>
+        <button
+            onClick={toggleTheme}
+            className="w-10 h-10 inline-flex items-center justify-center shrink-0"
+        >
+            <img
+                src={theme.name === "light" ? darkIcon : lightIcon}
+                alt="theme icon"
+                className="w-6 h-6 block"
+            />
+        </button>
     );
 }
